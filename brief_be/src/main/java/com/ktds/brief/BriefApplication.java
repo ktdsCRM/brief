7f2b9fd7ea95e388ac5dfe6af1573de42f7be731
annamon <annamon20@likelion.org>
2021-11-15 16:02:46 +0900

    [Feat] BE 프로젝트 생성

---
+++ brief_be/src/main/java/com/ktds/brief/BriefApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.brief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefApplication.class, args);
+	}
+
+}
---
+++ brief_be/src/test/java/com/ktds/brief/BriefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.brief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
