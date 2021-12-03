476660bcea06610f1237e33a6fd5b2f2060f5f6b
gihye0395 <gihye0395@gmail.com>
2021-12-03 11:02:27 +0900

    [Feat] FE : 텍스트요약 컴포넌트 css/html 구현

---
+++ brief_fe/src/layout/Middle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TextSummary/>
+  </div>
+</template>
+
+<script>
+import TextSummary from "../components/middle/TextSummary.vue";
+export default {
+  name: "Middle",
+  components: {TextSummary},
+};
+</script>
+
+<style>
+</style>
