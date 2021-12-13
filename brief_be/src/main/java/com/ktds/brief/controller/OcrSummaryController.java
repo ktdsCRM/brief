8f5116ca65a8d46d50a0dcd42bddf804b213d8bb
annamon <annamon20@likelion.org>
2021-12-13 15:22:38 +0900

    [Feat] BE : 요약 api 구현

--- brief_be/src/main/java/com/ktds/brief/controller/OcrSummaryController.java
+++ brief_be/src/main/java/com/ktds/brief/controller/OcrSummaryController.java
@@ -1,14 +1,29 @@
 package com.ktds.brief.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ktds.brief.service.TextSummaryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @CrossOrigin("*")
+@RequestMapping("/textSummary")
 @RestController
 public class OcrSummaryController {
 
-	public static void main(String[] args) {
-		
+	final private TextSummaryService testSummaryService;
+	
+	@PostMapping("/sum")
+	public String textSum(@RequestBody String text) {
+		String s = text;
+		return text;
 	}
 
 }
