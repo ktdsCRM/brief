6ffc2623f4d8f97ce387c5063edf758db848f245
DESKTOP-EP4IMNP\82105 <sjsj9368@gmail.com>
2021-12-27 17:51:39 +0900

    [FEAT] FE:OCR_TextToSummary

--- brief_be/src/main/java/com/ktds/brief/controller/OcrSummaryController.java
+++ brief_be/src/main/java/com/ktds/brief/controller/OcrSummaryController.java
@@ -1,9 +1,14 @@
 package com.ktds.brief.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ktds.brief.service.TextSummaryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +17,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/ocr")
 @RestController
 public class OcrSummaryController {
+	
+	final private TextSummaryService textSummaryService;
 
-
+	
+	//텍스트 요약
+	@RequestMapping(value = "/sum", method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json; charset=utf-8")
+	public Object textSummary(@RequestBody String input) throws Exception{
+		String textRes = textSummaryService.getTextSum(input);
+		return new ResponseEntity<>(textRes, HttpStatus.OK);
+		
+	}
 
 }
