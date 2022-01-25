7b5fbe63b65577b5599d5a5488f5f2bfb903e5d9
annamon <annamon20@likelion.org>
2022-01-26 07:26:11 +0900

    [Feat] FE : 기능별 input data 조건 추가

--- brief_fe/src/components/middle/OCRSummary.vue
+++ brief_fe/src/components/middle/OCRSummary.vue
@@ -3,9 +3,14 @@
     <div class="inputText">
       <div>
         <div class="infoText">
-          <font-awesome-icon :icon="['far', 'file-image']" size="2x" style="vertical-align:middle;" />
+          <font-awesome-icon
+            :icon="['far', 'file-image']"
+            size="2x"
+            style="vertical-align: middle"
+          />
           <span>&nbsp;&nbsp;사진 파일을 첨부해주세요.</span>
         </div>
+        <div class="typeInfo">* .png, .jpg, .jpeg / 1MB 이하만 가능합니다.</div>
         <div>
           <input
             type="file"
@@ -39,16 +44,16 @@
         </p>
       </div>
       <div v-else-if="extract === 'waiting'" class="outputTextBox">
-        <p style="color:#808080" align="justify">
-          {{"... 사진에서 텍스트를 추출하는 중입니다."}}
+        <p style="color: #808080" align="justify">
+          {{ "... 사진에서 텍스트를 추출하는 중입니다." }}
         </p>
       </div>
       <div v-else class="outputTextBox">
-        <p style="color:#808080" align="justify">
-          {{"추출된 내용이 없습니다."}}
+        <p style="color: #808080" align="justify">
+          {{ "추출된 내용이 없습니다." }}
         </p>
       </div>
-      <div class="sentenceLength" style="margin-top:4pt;">
+      <div class="sentenceLength" style="margin-top: 4pt">
         <span v-if="extract === 'result'"> {{ this.export.length }}자 </span>
       </div>
     </div>
@@ -70,18 +75,18 @@
           </p>
         </div>
         <div v-else-if="show === 'waiting'" class="outputTextBox">
-        <p style="color:#808080" align="justify">
-          {{"... 입력된 내용을 요약하는 중입니다."}}
-        </p>
-      </div>
-      <div v-else class="outputTextBox">
-        <p style="color:#808080" align="justify">
-          {{"요약된 내용이 없습니다."}}
-        </p>
-      </div>
-      <div class="sentenceLength" style="margin-top:4pt;">
-        <span v-if="show === 'result'"> {{ this.output.length }}자 </span>
-      </div>
+          <p style="color: #808080" align="justify">
+            {{ "... 입력된 내용을 요약하는 중입니다." }}
+          </p>
+        </div>
+        <div v-else class="outputTextBox">
+          <p style="color: #808080" align="justify">
+            {{ "요약된 내용이 없습니다." }}
+          </p>
+        </div>
+        <div class="sentenceLength" style="margin-top: 4pt">
+          <span v-if="show === 'result'"> {{ this.output.length }}자 </span>
+        </div>
       </div>
     </div>
   </div>
@@ -107,77 +112,91 @@ export default {
     //사진선택
     selectFile(e) {
       const file = e.target.files[0];
-      const imageSrc = window.URL.createObjectURL(file)
-      console.log(imageSrc)
+      const imageSrc = window.URL.createObjectURL(file);
+      console.log(imageSrc);
       //1. 파일 확장자 alert
-      this.fileName = file['name'];
+      this.fileName = file["name"];
       //파일의 확장자 추출
-      var fileDot = this.fileName.split('.').pop()
+      var fileDot = this.fileName.split(".").pop();
       //가능한 확장자
-      var dotArray = ["bmp", "dib", "jpeg", "jpg", "jpe", "jp2", "png", "webp", "pbm", "pgm", "ppm", "sr", "ras", "tiff", "tif"];
-      if(dotArray.includes(fileDot)==false){
-        alert(fileDot+' 파일은 업로드 하실 수 없습니다.');
-        this.$refs.imageFileInput.value = '';
+      var dotArray = [
+        "bmp",
+        "dib",
+        "jpeg",
+        "jpg",
+        "jpe",
+        "jp2",
+        "png",
+        "webp",
+        "pbm",
+        "pgm",
+        "ppm",
+        "sr",
+        "ras",
+        "tiff",
+        "tif",
+      ];
+      if (dotArray.includes(fileDot) == false) {
+        alert(fileDot + " 파일은 업로드 하실 수 없습니다.");
+        this.$refs.imageFileInput.value = "";
       }
       //2. 파일 미리 보기
       var reader = new FileReader();
-      document.getElementById('preview').src = imageSrc;
+      document.getElementById("preview").src = imageSrc;
       reader.readAsDataURL(e.target.files[0]);
     },
     //추출
     fileUpload() {
-      this.output = "",
-      this.show = "";
+      (this.output = ""), (this.show = "");
       let formData = new FormData();
       let imgFile = document.getElementById("imageFileInput");
-      if(imgFile.files[0]===undefined){
+      if (imgFile.files[0] === undefined) {
         alert("파일을 첨부해주세요.");
-      }else{
+      } else {
         this.extract = "waiting";
         formData.append("image", imgFile.files[0]);
-      axios
-        .post(`${SERVER.ROUTES.OCR}`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `KakaoAK ${process.env.VUE_APP_OCR_REST_API_KEY}`,
-          },
-        })
-        .then((response) => {
-          this.export = "";
-          this.extract = "result";
-          for (var i = 0; i < response.data["result"].length; i++) {
-            this.export += response.data["result"][i]["recognition_words"][0];
-          }
-        });
+        axios
+          .post(`${SERVER.ROUTES.OCR}`, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: `KakaoAK ${process.env.VUE_APP_OCR_REST_API_KEY}`,
+            },
+          })
+          .then((response) => {
+            this.export = "";
+            this.extract = "result";
+            for (var i = 0; i < response.data["result"].length; i++) {
+              this.export += response.data["result"][i]["recognition_words"][0];
+            }
+          });
       }
     },
     //새로고침
     reload() {
       (this.input = ""),
-      (this.output = ""),
-      (this.show = ""),
-      (this.export = ""),
-      (this.extract = "");
-      (this.$refs.imageFileInput.value = '');
-      (document.getElementById('preview').src = '');
+        (this.output = ""),
+        (this.show = ""),
+        (this.export = ""),
+        (this.extract = "");
+      this.$refs.imageFileInput.value = "";
+      document.getElementById("preview").src = "";
     },
 
     check() {
       let err = true;
       let msg = "";
-      err && !this.export && (
-        (msg = "추출하기를 먼저 진행해주세요."),
-        (err = false)
-      );
-      if(!err) alert(msg);
-      else(this.show = "waiting"), this.send();
+      err &&
+        !this.export &&
+        ((msg = "추출하기를 먼저 진행해주세요."), (err = false));
+      if (!err) alert(msg);
+      else (this.show = "waiting"), this.send();
     },
     //요약
     send() {
       http
         .post("ocr/sum", {
           input: this.export,
-          filename: this.fileName
+          filename: this.fileName,
         })
         .then((response) => {
           (this.show = "result"), (this.output = response.data);
@@ -193,13 +212,16 @@ export default {
   height: 70%;
   margin-top: 3%;
 }
-.preview { 
-  width: 200px; 
+.preview {
+  width: 200px;
   height: auto;
   max-width: 200px;
   max-height: 200px;
-  border:1px solid #eaeaea; 
-  padding: 0px; 
+  border: 1px solid #eaeaea;
+  padding: 0px;
+}
+.typeInfo {
+  padding-bottom: 20px;
 }
 .imageFileInput {
   height: 45px;
